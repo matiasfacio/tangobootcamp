3881cd218cb097af3228914a87a08d3904fffa1b
Matias Facio <matiaspersonal@gmail.com>
2021-08-28 17:25:22 +0300

    updated mutations and error handler

--- src/backend/UpdateUserInformation.tsx
+++ src/backend/UpdateUserInformation.tsx
@@ -1,11 +1,55 @@
 import React from "react";
 import styled from "styled-components";
-import { Form, Input } from "antd";
+import { Form, Input, Modal } from "antd";
 import { useUserMutations } from "../util/useUserMutations";
 import { PrimaryButton } from "../components/UIComponents/PrimaryButton";
+import { useQueryClient } from "react-query";
+
+const OnResult = ({
+  onOk,
+  visible,
+  message,
+}: {
+  onOk: () => void;
+  visible: boolean;
+  message: string;
+}) => {
+  return (
+    <Modal
+      visible={visible}
+      title={"Message"}
+      footer={<PrimaryButton onClick={() => onOk()}>Ok</PrimaryButton>}
+    >
+      {message}
+    </Modal>
+  );
+};
 
 export const UpdateUserInformation = ({ user }) => {
-  const userUpdate = useUserMutations(user);
+  const [success, setSuccess] = React.useState<boolean>(null);
+  const [error, setError] = React.useState(false);
+  const queryClient = useQueryClient();
+  const options = {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["user", user.email]);
+      setSuccess(data.ok);
+    },
+    onError: (data) => {
+      console.log("data", data);
+      setError(!data.ok);
+    },
+  };
+
+  const onOk = () => {
+    if (success) {
+      setSuccess(false);
+    }
+    if (error) {
+      setError(false);
+    }
+  };
+
+  const userUpdate = useUserMutations(user, options);
 
   const layout = {
     labelCol: { span: 8 },
@@ -49,6 +93,18 @@ export const UpdateUserInformation = ({ user }) => {
           <PrimaryButton type="submit">Submit</PrimaryButton>
         </Form.Item>
       </Form>
+      <OnResult
+        onOk={onOk}
+        visible={success}
+        message={"Your information was correctly updated."}
+      />
+      <OnResult
+        onOk={onOk}
+        visible={error}
+        message={
+          "Error: there was an error updating your data. We will look at it as soon as possible."
+        }
+      />
     </Container>
   );
 };
--- src/util/useUserMutations.tsx
+++ src/util/useUserMutations.tsx
@@ -1,36 +1,25 @@
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 
-export const useUserMutations = (user) => {
-  const queryClient = useQueryClient();
+export const useUserMutations = (user, options) => {
   const BASE_URL = "https://tbc.tangodefinitions.com/api/user";
   // const BASE_URL = "http://localhost:5000/api/user";
   return useMutation(
     ["user", user.email],
     async (newUser) => {
-      try {
-        const result = await fetch(`${BASE_URL}/update`, {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newUser),
-        });
-        const data = await result.json();
-
-        if (result.ok) {
-          return data;
-        } else {
-          throw new Error("user dosn't exist");
-        }
-      } catch (error) {
-        return error;
+      const result = await fetch(`${BASE_URL}/update`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+      if (result.ok) {
+        return result;
+      } else {
+        throw Error("Error fetching data");
       }
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(["user", user.email]);
-      },
-    }
+    options
   );
 };
